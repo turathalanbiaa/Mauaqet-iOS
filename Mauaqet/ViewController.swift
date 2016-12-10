efd3b905ab9d556473e79d559cb06a4cc20e350c
Turath-Alanbiaa <turath-alanbiaa@turath-alanbiaas-macbook-air.local>
2016-12-10 10:34:42 +0300

    Initial Commit

---
+++ Mauaqet/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Mauaqet
+//
+//  Created by Turath-Alanbiaa on 12/10/16.
+//  Copyright © 2016 Turath-Alanbiaa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
